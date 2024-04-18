39f6abba5fdeeccddce9a25cecc76043febade74
emapsp22@iCloud.com <efranco3667@arecibo.inter.edu>
2024-04-18 11:39:11 -0400

    update

---
+++ Example 5/js/codigo.js
@@ -0,0 +1,17 @@
+document.getElementById("especial").addEventListener("Click", function(e){
+    alert("Hola perro!");
+});
+
+document.getElementById("saludo").addEventListener("Click", function (e){
+
+let h1 = document.querySelector("h1");
+h1.style.color = "Red";
+h1.style.fontFamily = "Arial";
+h1.style.position = "absolute";
+h1.style.zIndex = "100%";
+h1.style.bottom = "50%";
+h1.style.padding = "33px";
+h1.style.backgroundColor = "Red"
+
+
+});
